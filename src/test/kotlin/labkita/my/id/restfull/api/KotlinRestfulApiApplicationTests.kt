3781545cc0bdc003162ccc01965527d24b64947e
fajar sidik priatna <fajar@suitmedia.com>
2023-09-04 11:38:56 +0700

    initial

---
+++ src/test/kotlin/labkita/my/id/restfull/api/KotlinRestfulApiApplicationTests.kt
@@ -0,0 +1,13 @@
+package labkita.my.id.restfull.api
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class KotlinRestfulApiApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
